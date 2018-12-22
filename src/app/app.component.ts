c41c7bd3189b2fa3d4a812a369b5e4d59bb6c6cd
Eugene <Creepis2011@gmail.com>
2018-12-22 17:04:39 +0200

    lesson 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ApiService} from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  list = [];
+  constructor(private api: ApiService) {}
+  get() {
+    this.api.get().subscribe((res: any) => {
+      this.list = res;
+    });
+  }
+  post() {
+    const test = {title: 'Kekekekeke', description: 'Yaaaaa dudoseeeeeeeeeeer'};
+    for (let i = 0; i < 100; i++) {
+        this.api.post(test).subscribe((res: any) => {
+            this.list.push(test);
+        });
+    }
+  }
+  del(id, index) {
+      console.log(index);
+      this.api.del(id).subscribe((res: any) => {
+        this.list.splice(index, 1);
+      });
+  }
+  update() {
+  }
 }
